ee9ea225a44d89d97a279f74875632a21c9616c5
thomasjl <thomas.jean-louis@capgemini.com>
2025-03-17 16:32:38 +0100

    add missing var node_disk_size_gb

--- main.tf
+++ main.tf
@@ -5,4 +5,5 @@ module "gke" {
   environment       = var.environment
   node_count        = var.node_count
   node_machine_type = var.node_machine_type
+  node_disk_size_gb = var.node_disk_size_gb
 }
