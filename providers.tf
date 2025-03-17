08d1dfd3b7439b15621cd71fe71fadb94034f92d
thomasjl <thomas.jean-louis@capgemini.com>
2025-03-17 16:19:27 +0100

    remove token from gke module

--- providers.tf
+++ providers.tf
@@ -18,6 +18,5 @@ provider "google" {
 
 provider "kubernetes" {
   host                   = "https://${module.gke.endpoint}"
-  token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(module.gke.ca_certificate)
 }
