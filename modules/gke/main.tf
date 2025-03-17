5e7de9ef3bca4111a43a83703fedcd1f2fce0f09
thomasjl <thomas.jean-louis@capgemini.com>
2025-03-17 16:39:37 +0100

    set var.node_disk_size_gb in gke cluster

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -5,6 +5,10 @@ resource "google_container_cluster" "primary" {
   remove_default_node_pool = true
   initial_node_count       = 1
 
+  node_config {
+    disk_size_gb = var.node_disk_size_gb
+  }
+
   lifecycle {
     prevent_destroy = true
   }
